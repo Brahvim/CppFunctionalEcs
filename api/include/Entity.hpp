8f0a6a4ade317563a82968d5d65f3690e72b4afb
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-04-19 12:41:30 +0530

    I'm crazy.

--- api/include/Entity.hpp
+++ api/include/Entity.hpp
@@ -4,8 +4,11 @@ namespace Ecs {
 
     using entity_t = size_t;
 
-    // Memory management:
+#pragma region // Memory management:
     entity_t create_entity();
     void destroy_entity(entity_t entity);
+#pragma endregion
+
+    inline size_t get_num_entities();
 
 }
--- api/src/Entity.cpp
+++ api/src/Entity.cpp
@@ -1,26 +1,25 @@
+#include <memory>
+
 #include "Entity.hpp"
-#include <vector>
 
 namespace Ecs {
 
     static size_t s_num_entities = 0;
-    static std::vector<Ecs::entity_t> s_entities;
-    static std::vector<Ecs::entity_t> s_free_entities;
 
-    // Memory management:
+    // Free list management:
+    static size_t s_num_free_entities = 0;
+    static size_t *s_free_entities_list_length = 0;
+    static Ecs::entity_t *s_free_entities_list = (Ecs::entity_t*)std::calloc(24, sizeof(Ecs::entity_t));
+
+#pragma region // Memory management:
     inline Ecs::entity_t create_entity() {
-        if (s_free_entities.empty()) {
-            // I have a really, REALLY good idea that eliminates this branch!
-        } else {
-            const Ecs::entity_t to_ret = s_free_entities.back();
-            s_free_entities.pop_back();
-            return to_ret;
-        }
+        if (s_num_free_entities == 0);
     }
 
-    inline void destroy_entity(const Ecs::entity_t entity) {
+    inline void destroy_entity(const Ecs::entity_t p_entity) {
 
     }
+#pragma endregion
 
     inline size_t get_num_entities() {
         return s_num_entities;
