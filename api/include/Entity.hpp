a5df33390178c16e676fd336cf94fb3568dc6070
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-04-19 13:25:22 +0530

    SCREW IT! I'm sticking to C++ FOR NOW. STL elimination will come! Later.

--- api/include/Entity.hpp
+++ api/include/Entity.hpp
@@ -1,14 +1,16 @@
-#include <stddef.h>
+#include <cstddef>
 
-namespace Ecs {
+namespace ecs {
 
     using entity_t = size_t;
+    typedef void(*entity_limit_broken_cbck_t)();
 
 #pragma region // Memory management:
     entity_t create_entity();
     void destroy_entity(entity_t entity);
+    void set_entity_limit_broken_cbck(entity_limit_broken_cbck_t function);
 #pragma endregion
 
-    inline size_t get_num_entities();
+    size_t get_num_entities();
 
 }
--- api/src/Entity.cpp
+++ api/src/Entity.cpp
@@ -1,27 +1,38 @@
-#include <memory>
+#include <vector>
+#include <iostream>
 
 #include "Entity.hpp"
 
-namespace Ecs {
+namespace ecs {
 
     static size_t s_num_entities = 0;
-
-    // Free list management:
-    static size_t s_num_free_entities = 0;
-    static size_t *s_free_entities_list_length = 0;
-    static Ecs::entity_t *s_free_entities_list = (Ecs::entity_t*)std::calloc(24, sizeof(Ecs::entity_t));
+    static std::vector<ecs::entity_t> s_free_entities;
+    static entity_limit_broken_cbck_t s_entity_limit_broken_cbck = [] {
+        std::cout << "Entity limit of `" << __SIZE_MAX__ << "` entities broken!" << std::endl;
+    };
 
 #pragma region // Memory management:
-    inline Ecs::entity_t create_entity() {
-        if (s_num_free_entities == 0);
+    inline ecs::entity_t create_entity() {
+        if (s_free_entities.empty()) {
+            if (s_num_entities <= __SIZE_MAX__)
+                return ++s_num_entities;
+            else {
+                s_entity_limit_broken_cbck();
+                return 0;
+            }
+        } else {
+            const ecs::entity_t to_ret = s_free_entities.back();
+            s_free_entities.pop_back();
+            return to_ret;
+        }
     }
 
-    inline void destroy_entity(const Ecs::entity_t p_entity) {
-
+    inline void destroy_entity(const ecs::entity_t p_entity) {
+        s_free_entities.push_back(p_entity);
     }
 #pragma endregion
 
-    inline size_t get_num_entities() {
+    inline ecs::entity_t get_num_entities() {
         return s_num_entities;
     }
 
