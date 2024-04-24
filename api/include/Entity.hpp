355117a562aec45f4ea00c2e68ba89c0cf810657
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-04-24 13:52:14 +0530

    Fixed a bug, and... coming up with a new design. Yeah.
    Git broke :/

--- api/include/Entity.hpp
+++ api/include/Entity.hpp
@@ -8,6 +8,9 @@ namespace ecs {
 
     using entity_t = entity*;
     using destructor_cbck_t = void(*)();
+    const size_t component_flags_bits = 8;
+    const size_t component_flags_id_count = ecs::component_flags_bits / 2;
+    const size_t component_flags_group_count = ecs::component_flags_bits - ecs::component_flags_id_count;
     // typedef void(*destructor_cbck_t)(ecs::entity entity);
     // using destructor_cbck_t = void(*)(ecs::entity entity); // Memorize this, Brahvim! Memorize this!!1
 
@@ -23,22 +26,22 @@ namespace ecs {
     size_t get_num_entities();
 
 #pragma region // Callback management.
+    ecs::destructor_cbck_t detach_entity_destructor(ecs::entity_t entity);
     void attach_entity_destructor(ecs::entity_t entity, ecs::destructor_cbck_t destructor);
-    void detach_entity_destructor(ecs::entity_t entity, ecs::destructor_cbck_t destructor);
 #pragma endregion
 
     class entity {
 
     public:
-        std::bitset<4> component_flags;
+        std::bitset<ecs::component_flags_bits> component_flags;
 
     private:
         friend ecs::entity_t ecs::create_entity();
         friend void ecs::destroy_entity(entity_t entity);
+        friend ecs::destructor_cbck_t detach_entity_destructor(ecs::entity_t entity);
         friend void ecs::attach_entity_destructor(ecs::entity_t entity, ecs::destructor_cbck_t destructor);
-        friend void ecs::detach_entity_destructor(ecs::entity_t entity, ecs::destructor_cbck_t destructor);
 
-        ecs::destructor_cbck_t destructor;
+        ecs::destructor_cbck_t destructor_callback;
 
         entity() { }
 
--- api/src/Entity.cpp
+++ api/src/Entity.cpp
@@ -9,6 +9,8 @@ namespace ecs {
     static size_t s_num_entities = 0;
     static std::vector<ecs::entity_t> s_entities;
     static std::vector<ecs::entity_t> s_free_entities;
+    static int s_components_map[ecs::component_flags_group_count][component_flags_id_count];
+
     // static entity_limit_broken_cbck_t s_entity_limit_broken_cbck = [] {
     //     std::cout << "Limit of `" << ecs::max_entity_count << "` entities broken!" << std::endl;
     // };
@@ -35,15 +37,16 @@ namespace ecs {
     // }
 
     void destroy_entity(ecs::entity_t p_entity) {
+        if (p_entity != nullptr)
+            return;
+
         auto existing_entity_it = std::find(s_free_entities.begin(), s_free_entities.end(), p_entity);
         // Double-free check. Might be eliminated later LOL:
         if (existing_entity_it == s_free_entities.end()) {
             s_free_entities.push_back(p_entity); // Add it to the free list,
             // ...Call all callbacks that want to know when it gets destroyed
             // (probably components willing to auto-detach):
-            for (auto c : s_entities)
-                // { std::cout << "Called an entity destructor!" << std::endl;
-                c->destructor();
+            p_entity->destructor_callback();
         }
     }
 #pragma endregion
@@ -52,12 +55,16 @@ namespace ecs {
         return s_num_entities - s_free_entities.size();
     }
 
-    void attach_entity_destructor(ecs::entity_t p_entity, ecs::destructor_cbck_t p_destructor) {
-        p_entity->destructor = p_destructor;
+    ecs::destructor_cbck_t detach_entity_destructor(ecs::entity_t p_entity) {
+        if (p_entity == nullptr)
+            return nullptr;
+
+        return p_entity->destructor_callback;
     }
 
-    void detach_entity_destructor(ecs::entity_t p_entity, ecs::destructor_cbck_t p_destructor) {
-        p_entity->destructor = p_destructor;
+    void attach_entity_destructor(ecs::entity_t p_entity, ecs::destructor_cbck_t p_destructor) {
+        if (p_entity != nullptr)
+            p_entity->destructor_callback = p_destructor;
     }
 
 }
