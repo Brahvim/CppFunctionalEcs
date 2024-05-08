c55a70f204dac5c78306435d96cabe6e6a99285a
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-05-08 19:22:34 +0530

    ...Time for another attempt at making an ECS...

--- api/include/Component.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Entity.hpp"
-
-namespace ecs {
-
-    class component;
-
-}
--- test/src/Main.cpp
+++ test/src/Main.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
 
-#include "Entity.hpp"
-#include "FloatingComponent.hpp"
+#include "Test.hpp"
 
 int main() {
 
-    ecs::entity *e = ecs::create_entity();
-    std::cout << "Number of entities: `" << ecs::get_num_entities() << "`." << std::endl;
-
-    ecs::components::floating::attach(e, 9);
-    std::cout << ecs::components::floating::get(e) << std::endl;
-
-    ecs::components::floating::modify(e, 1);
-
-    std::cout << ecs::components::floating::get(e) << std::endl;
-    ecs::components::floating::detach(e);
-
-    ecs::destroy_entity(e);
-    std::cout << "Number of entities: `" << ecs::get_num_entities() << "`." << std::endl;
 }
