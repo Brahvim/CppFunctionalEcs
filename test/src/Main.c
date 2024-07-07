9da2a2000c82663635fbcd4e85e80522276879de
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-07-07 22:06:48 +0530

    `32`M RAM for a million entities. Huh.

--- test/src/Main.c
+++ test/src/Main.c
@@ -9,7 +9,7 @@ printf("`" #call "` (line `%d`): %s.\n", __LINE__, ecs_context_status_to_string(
 // printf("ECS `" #call "` call status [" __FILE__ ": %d ]: %s.\n", __LINE__, ecs_context_status_to_string(call))
 
 static struct ecs_context *s_ecs;
-static const size_t s_entity_count = 5;
+static const size_t s_entity_count = 1000;
 
 int main() {
     LOG_ECS_CALL(ecs_context_create(&s_ecs));
@@ -23,14 +23,17 @@ int main() {
 
     for (size_t i = 0; i < s_entity_count; i++) {
         // printf("Allocating entity `%zu`.\n", i);
-        LOG_ECS_CALL(ecs_entity_create(s_ecs, &(entities[i])));
+        // LOG_ECS_CALL(
+        ecs_entity_create(s_ecs, &(entities[i])) // )
+            ;
     }
 
     printf("ECS entity count: `%zu`.\n", ecs_context_entity_count(s_ecs));
     printf("ECS trimmed. Memory recovered: `%zu` bytes.\n", ecs_context_trim(s_ecs));
 
     for (size_t i = 0; i < s_entity_count; ++i) {
-        printf("Deallocating entity `%zu`.\n", i);
+        if (i % 1000 == 0)
+            printf("Deallocating entity `%zu`.\n", i);
         ecs_entity_destroy(s_ecs, entities[i]);
     }
 
